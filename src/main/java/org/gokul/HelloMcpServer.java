42d0252b4d3c1417931173981124d3ae2ce22a2a
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2026-08-13 23:28:14 +0200

    add json schema

--- src/main/java/org/gokul/HelloMcpServer.java
+++ src/main/java/org/gokul/HelloMcpServer.java
@@ -1,10 +1,10 @@
 package org.gokul;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.json.McpJsonMapper;
 import io.modelcontextprotocol.json.jackson2.JacksonMcpJsonMapper;
-import io.modelcontextprotocol.server.McpServer;
-import io.modelcontextprotocol.server.McpServerFeatures;
 import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema.Tool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +13,24 @@ public class HelloMcpServer {
     private static final Logger log = LoggerFactory.getLogger(HelloMcpServer.class);
 
     public static void main(String[] args) {
-        System.out.println("MCP SDK available: "+ McpServer.class.getName());
+
+        System.out.println("hello");
+        McpJsonMapper jsonMapper = new JacksonMcpJsonMapper(new ObjectMapper());
+        var transportProvider = new StdioServerTransportProvider(jsonMapper);
+
+        Tool echoTool = Tool.builder("echo", jsonMapper, """
+                        {
+                          "type": "object",
+                          "properties": {
+                            "message": {
+                              "type": "string",
+                              "description": "The text to echo back"
+                            }
+                          },
+                          "required": ["message"]
+                        }
+                        """)
+                .description("Returns whatever text you pass in. Useful for testing.")
+                .build();
     }
 }
